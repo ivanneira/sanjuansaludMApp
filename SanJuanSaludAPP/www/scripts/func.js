7c2521427064d5c7133562a506db9add1a17f602
ivanneira <ien1983@gmail.com>
2017-08-16 10:31:21 -0300

    initial commit

---
+++ SanJuanSaludAPP/www/scripts/func.js
@@ -0,0 +1,22 @@
+﻿
+var Apiurl = "http://10.64.65.200/AresApi/Api";
+var portalUrl = "http://sanjuan.gov.ar/gen/gobierno/app/noticias/salud/c/index.json";
+
+function getSlider()
+{
+    $.ajax({
+
+        url: portalUrl,
+        cache: false,
+        type: 'get',
+        dataType: "json",
+        success: function (response) {
+
+            console.dir(response);
+        },
+        error: function (error) {
+            console.log("Error: " + error)
+        }
+
+    })
+}
