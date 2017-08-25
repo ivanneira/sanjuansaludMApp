dbb9eacb3f7bc68a53b47d74446de90ea2227a5e
Sebassss <pseba20@gmail.com>
2017-08-25 12:51:21 -0300

    con caps details

--- SanJuanSaludAPP/www/scripts/func.js
+++ SanJuanSaludAPP/www/scripts/func.js
@@ -1,5 +1,5 @@
 ﻿
-var server = "200.0.236.210";
+var server = "gedoc.sanjuan.gov.ar";
 var ErrorAjax = "Ups! Hubo un problema con su conexión a internet.";
 var NoticiasURL = "http://"+server+"/AresApi/Api/Portal/Noticias";
 var DepartamentosURL = "http://"+server+"/AresApi/Api/Departamento";
