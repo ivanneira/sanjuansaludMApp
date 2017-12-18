400f8ba43c1f334f73ac54b4ff8c0ffd5ef08e1e
Sebassss <pseba20@gmail.com>
2017-12-18 07:42:50 -0300

    Servicio de Carta médica. settimeout perdida del evento click sobre el boton
    Doble tap para abrir noticia.
    
    Waiting sobre slide container.
    
    Formulario mil dias margenes.
    
    Evento boton carta medica
    
    Zona y Departamento en Detalles

--- SanJuanSaludAPP/www/scripts/view.js
+++ SanJuanSaludAPP/www/scripts/view.js
@@ -54,7 +54,38 @@ myApp.onPageInit('mapa', function (page) {
 function load()
 {
 
-    setTimeout(function(){
+
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+
+    //alert('Connection type: ' + states[networkState]);
+    if(states[networkState] == "No network connection")
+    {
+        $("#btnCartaMedica").unbind('click').click(function() {
+            window.plugins.toast.show("Se requiere una conexión activa para usar este servicio.", "3000", "bottom");
+        });
+        $("#btnCartaMedica").css('background-color','lightgray');
+    }
+    else
+    {
+        $("#btnCartaMedica").unbind('click').click(function() {
+            mainView.router.loadPage("cartaMedica.html");
+        });
+        $("#btnCartaMedica").css('background-color','white');
+
+    }
+
+        setTimeout(function(){
 
         $("#btnCartaMedica").unbind('click').click(function() {
             window.plugins.toast.show("Se requiere una conexión activa para usar este servicio.", "3000", "bottom");
